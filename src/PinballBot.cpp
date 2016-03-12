94a95f1a6cca49eaab48e9b3525af0a96c9688ec
Nico Hauser <me@tyratox.ch>
2016-03-12 18:05:34 +0100

    Structured main file

--- src/PinballBot.cpp
+++ src/PinballBot.cpp
@@ -1,15 +1,58 @@
 //============================================================================
 // Name        : PinballBot.cpp
-// Author      : 
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
+// Author      : Nico Hauser, David Schmid
+// Version     : 0.1
+// Description : A reinforcement learning agent that learns to play pinball
 //============================================================================
 
 #include <iostream>
-using namespace std;
+
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+/**
+ * Contains all the information of a state
+ */
+
+class State{
+
+};
+
+/**
+ * Gets the expected reward based on a lookup table and the model of the environment
+ * @param	s			State		The State of which the reward is calculated
+ * @return				double		The expected reward
+ */
+
+double reward(State s){
+
+
+
+	return 0.0;
+}
+
+/**
+ * Calculates the expected value the given state will in the next turn and in the future.
+ * Policies, the reward function and the model of the environment are used to do this calculation.
+ * @param	s			State		The State of which the value is calculated
+ * @return				double		The expected value
+ */
+
+double value(State s){
+
+
+	return 0.0;
+}
 
 int main() {
-	cout << "" << endl; // prints 
+
+	boost::numeric::ublas::vector<double> v(3);
+
+	v[0] = 10;
+
+	std::cout << v[0] << std::endl;
+
+
+
 	return 0;
 }
