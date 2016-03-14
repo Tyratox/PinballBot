2de5fb0b89456483e0371c9499bfb0e017eb5ab5
Nico Hauser <me@tyratox.ch>
2016-03-14 08:37:35 +0100

    Changed variable names and changed reward and value functions to member functions of the "State" class

--- src/PinballBot.cpp
+++ src/PinballBot.cpp
@@ -17,9 +17,9 @@
 class Ball{
 	private:
 
-			boost::numeric::ublas::vector<double> ball_position;
-			boost::numeric::ublas::vector<double> ball_velocity;
-			boost::numeric::ublas::vector<double> ball_acceleration;
+			boost::numeric::ublas::vector<double> position;
+			boost::numeric::ublas::vector<double> velocity;
+			boost::numeric::ublas::vector<double> acceleration;
 
 			/**
 			 * Checks if a given vector is valid to use
@@ -42,29 +42,27 @@ class Ball{
 			 * Constructor without any arguments, defaults to 3-dimensional vector
 			 */
 			Ball(){
-				this->set_ball_position(boost::numeric::ublas::vector<double>(3));
-				this->set_ball_velocity(boost::numeric::ublas::vector<double>(3));
-				this->set_ball_acceleration(boost::numeric::ublas::vector<double>(3));
+				this->Ball(3);
 			}
 
 			Ball(int dimensions){
 				if(this->is_vector_valid(boost::numeric::ublas::vector<double>(dimensions))){
-					this->set_ball_position(boost::numeric::ublas::vector<double>(dimensions));
-					this->set_ball_velocity(boost::numeric::ublas::vector<double>(dimensions));
-					this->set_ball_acceleration(boost::numeric::ublas::vector<double>(dimensions));
+					this->set_position(boost::numeric::ublas::vector<double>(dimensions));
+					this->set_velocity(boost::numeric::ublas::vector<double>(dimensions));
+					this->set_acceleration(boost::numeric::ublas::vector<double>(dimensions));
 				}
 			}
 
 			/**
 			 * Updates the ball position
-			 * @param	ball_position	boost::numeric::ublas::vector<double>	The new ball position
-			 * @return					bool									True if the variable was successfully updated,
-			 * 																	false if an error occurred
+			 * @param	position	boost::numeric::ublas::vector<double>	The new ball position
+			 * @return				bool									True if the variable was successfully updated,
+			 * 																false if an error occurred
 			 */
-			bool set_ball_position(boost::numeric::ublas::vector<double> ball_position){
+			bool set_position(boost::numeric::ublas::vector<double> position){
 
-				if(this->is_vector_valid(ball_position)){
-					this->ball_position = ball_position;
+				if(this->is_vector_valid(position)){
+					this->position = position;
 
 					return true;
 				}
@@ -76,19 +74,19 @@ class Ball{
 			 * Retrieves the current ball position
 			 * @return	boost::numeric::ublas::vector<double>	The current ball position
 			 */
-			boost::numeric::ublas::vector<double> get_ball_position(){
-				return this->ball_position;
+			boost::numeric::ublas::vector<double> get_position(){
+				return this->position;
 			}
 
 			/**
 			 * Updates the ball velocity
-			 * @param	ball_velocity	boost::numeric::ublas::vector<double>	The new ball velocity
+			 * @param	velocity	boost::numeric::ublas::vector<double>	The new ball velocity
 			 * @return					bool									True if the variable was successfully updated,
 			 * 																	false if an error occurred
 			 */
-			bool set_ball_velocity(boost::numeric::ublas::vector<double> ball_velocity){
-				if(this->is_vector_valid(ball_velocity)){
-					this->ball_velocity = ball_velocity;
+			bool set_velocity(boost::numeric::ublas::vector<double> velocity){
+				if(this->is_vector_valid(velocity)){
+					this->velocity = velocity;
 
 					return true;
 				}
@@ -100,19 +98,19 @@ class Ball{
 			 * Retrieves the current ball velocity
 			 * @return	boost::numeric::ublas::vector<double>	The current ball velocity
 			 */
-			boost::numeric::ublas::vector<double> get_ball_velocity(){
-				return this->ball_velocity;
+			boost::numeric::ublas::vector<double> get_velocity(){
+				return this->velocity;
 			}
 
 			/**
 			 * Updates the ball position
-			 * @param	ball_acceleration	boost::numeric::ublas::vector<double>	The new ball acceleration
+			 * @param	acceleration	boost::numeric::ublas::vector<double>	The new ball acceleration
 			 * @return						bool									True if the variable was successfully updated,
 			 * 																		false if an error occurred
 			 */
-			bool set_ball_acceleration(boost::numeric::ublas::vector<double> ball_acceleration){
-				if(this->is_vector_valid(ball_acceleration)){
-					this->ball_acceleration = ball_acceleration;
+			bool set_acceleration(boost::numeric::ublas::vector<double> acceleration){
+				if(this->is_vector_valid(acceleration)){
+					this->acceleration = acceleration;
 
 					return true;
 				}
@@ -124,8 +122,8 @@ class Ball{
 			 * Retrieves the current ball acceleration
 			 * @return	boost::numeric::ublas::vector<double>	The current ball acceleration
 			 */
-			boost::numeric::ublas::vector<double> get_ball_acceleration(){
-				return this->ball_acceleration;
+			boost::numeric::ublas::vector<double> get_acceleration(){
+				return this->acceleration;
 			}
 };
 
@@ -135,33 +133,39 @@ class Ball{
 
 class State{
 
+	private:
+
+			std::vector<Ball> balls;
+
+	public:
+
+		/**
+		 * Gets the expected reward based on a lookup table and the model of the environment
+		 * @param	s	State	The State of which the reward is calculated
+		 * @return		double	The expected reward
+		 */
+
+		double reward(State s){
+
+
+
+			return 0.0;
+		}
+
+		/**
+		 * Calculates the expected value the given state will in the next turn and in the future.
+		 * Policies, the reward function and the model of the environment are used to do this calculation.
+		 * @param	s	State	The State of which the value is calculated
+		 * @return		double	The expected value
+		 */
+
+		double value(State s){
+
+
+			return 0.0;
+		}
+
 };
-
-/**
- * Gets the expected reward based on a lookup table and the model of the environment
- * @param	s	State	The State of which the reward is calculated
- * @return		double	The expected reward
- */
-
-double reward(State s){
-
-
-
-	return 0.0;
-}
-
-/**
- * Calculates the expected value the given state will in the next turn and in the future.
- * Policies, the reward function and the model of the environment are used to do this calculation.
- * @param	s	State	The State of which the value is calculated
- * @return		double	The expected value
- */
-
-double value(State s){
-
-
-	return 0.0;
-}
 
 int main() {
 
